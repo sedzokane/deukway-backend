661534e12c64f7d790dd203ab290fd69c5eaa8d9
sedzokane <seydoukane015@gmail.com>
2026-06-28 12:46:37 +0000

    fix paydunya setup config

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -3,26 +3,21 @@ import { PrismaService } from '../prisma/prisma.service';
 
 const paydunya = require('paydunya');
 
-const setup = new paydunya.Setup({
-  masterKey: 'TdBzwUdV-vemt-YVWH-ZgqH-v3cF7WOr60kY',
-  privateKey: 'test_private_ZoNhhFEytY0dJhsUZAp9PUxZaOk',
-  publicKey: 'test_public_1FRdKfkjhI5F8dqQQlNhKPmqUn6',
-  token: 'KpVf7J0eswO6CbMe5OHk',
-  mode: 'test',
-});
-
-const store = new paydunya.Store({
-  name: 'Deukway',
-  tagline: 'Location immobiliere au Senegal',
-  phoneNumber: '338000000',
-  postalAddress: 'Dakar, Senegal',
-  websiteUrl: 'https://deukway-backend-production.up.railway.app',
-  logoUrl: 'https://res.cloudinary.com/dpnspgnpy/image/upload/v1/deukway/logo.png',
-});
-
 @Injectable()
 export class PaymentsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {
+    paydunya.Setup.masterKey = 'TdBzwUdV-vemt-YVWH-ZgqH-v3cF7WOr60kY';
+    paydunya.Setup.privateKey = 'test_private_ZoNhhFEytY0dJhsUZAp9PUxZaOk';
+    paydunya.Setup.publicKey = 'test_public_1FRdKfkjhI5F8dqQQlNhKPmqUn6';
+    paydunya.Setup.token = 'KpVf7J0eswO6CbMe5OHk';
+    paydunya.Setup.mode = 'test';
+
+    paydunya.Store.name = 'Deukway';
+    paydunya.Store.tagline = 'Location immobiliere au Senegal';
+    paydunya.Store.phoneNumber = '338000000';
+    paydunya.Store.postalAddress = 'Dakar, Senegal';
+    paydunya.Store.websiteUrl = 'https://deukway-backend-production.up.railway.app';
+  }
 
   async createInvoice(userId: string, amount: number, description: string, returnUrl: string) {
     try {
@@ -38,7 +33,7 @@ export class PaymentsService {
         invoice.create((err: any, response: any) => {
           if (err) {
             console.error('PayDunya error:', err);
-            reject(err);
+            reject(new Error(err));
           } else {
             console.log('PayDunya response:', JSON.stringify(response));
             resolve(response);
